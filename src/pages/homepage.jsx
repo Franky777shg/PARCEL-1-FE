c9ff02644b176d04e439132820d0ebeb08bd0f4c
dimassusetyo <dimassusetyo11@gmail.com>
2021-10-25 22:46:25 +0700

    user can filter products

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -23,10 +23,19 @@ class Homepage extends Component {
       currentPage: "",
       parcelPerPage: "",
       active: 1,
-      totalParcels: ""
+      totalParcels: "",
+      filterID: []
     }
   }
   componentDidMount() {
+    axios.get(`http://localhost:2000/homepage/getHomepage/${1}`)
+      .then(res => {
+        this.setState({ ...this.state, ...res.data })
+        // console.log(this.state)
+      })
+      .catch(err => console.log(err))
+  }
+  onResetFilter = () => {
     axios.get(`http://localhost:2000/homepage/getHomepage/${1}`)
       .then(res => {
         this.setState({ ...this.state, ...res.data })
@@ -44,32 +53,99 @@ class Homepage extends Component {
       })
       .catch(err => console.log(err))
   }
+  onFilter = () => {
+    if (this.state.filterID.length !== 0) {
+      const body = {
+        "idProductCategory": this.state.filterID
+      }
+      axios.post(`http://localhost:2000/homepage/filterParcel/1`, body)
+        .then(res => {
+          this.setState({ ...this.state, ...res.data, active: 1 })
+        })
+        .catch(err => console.log(err))
+    } else {
+      axios.get(`http://localhost:2000/homepage/getHomepage/1`)
+        .then(res => {
 
-  render() {
-    const { currentPage, parcelPerPage, totalParcels } = this.state
+          this.setState({ ...this.state, ...res.data, active: 1 })
+        })
+    }
+  }
+  onChangeFilter = (value) => {
+    const { filterID } = this.state
+    let filterIDNew = []
+    if (filterID.includes(value)) {
+      filterIDNew = filterID.filter(item => {
+        return item !== value
+      })
+      // console.log(filterIDNew)
+    }
+    else { filterIDNew = [...filterID, value] }
+    this.setState({ filterID: filterIDNew }, () => this.onFilter())
+  }
 
-    //pindah ke halaman tertentu
-    const paginate = (pageNum) => {
+  //pindah ke halaman tertentu
+  paginate = (pageNum) => {
+    if (this.state.filterID.length !== 0) {
+      const body = {
+        "idProductCategory": this.state.filterID
+      }
+      axios.post(`http://localhost:2000/homepage/filterParcel/${pageNum}`, body)
+        .then(res => {
+          this.setState({ ...this.state, ...res.data, active: pageNum })
+        })
+        .catch(err => console.log(err))
+    } else {
       axios.get(`http://localhost:2000/homepage/getHomepage/${pageNum}`)
         .then(res => {
 
           this.setState({ ...this.state, ...res.data, active: pageNum })
         })
     }
-    //halaman selanjutnya
-    const nextPage = () => {
+  }
+  //halaman selanjutnya
+  nextPage = () => {
+    const { currentPage } = this.state
+    if (this.state.filterID.length !== 0) {
+      const body = {
+        "idProductCategory": this.state.filterID
+      }
+      axios.post(`http://localhost:2000/homepage/filterParcel/${currentPage + 1}`, body)
+        .then(res => {
+          this.setState({ ...this.state, ...res.data, active: currentPage + 1 })
+        })
+        .catch(err => console.log(err))
+    }
+    else {
       axios.get(`http://localhost:2000/homepage/getHomepage/${currentPage + 1}`)
         .then(res => {
           this.setState({ ...this.state, ...res.data, active: currentPage + 1 })
         })
     }
-    //halaman sebelumnya
-    const prevPage = () => {
+  }
+  //halaman sebelumnya
+  prevPage = () => {
+    const { currentPage } = this.state
+    if (this.state.filterID.length !== 0) {
+      const body = {
+        "idProductCategory": this.state.filterID
+      }
+      axios.post(`http://localhost:2000/homepage/filterParcel/${currentPage - 1}`, body)
+        .then(res => {
+          this.setState({ ...this.state, ...res.data, active: currentPage - 1 })
+        })
+        .catch(err => console.log(err))
+    } else {
       axios.get(`http://localhost:2000/homepage/getHomepage/${currentPage - 1}`)
         .then(res => {
           this.setState({ ...this.state, ...res.data, active: currentPage - 1 })
         })
     }
+  }
+  render() {
+    // console.log(this.state.filterID)
+    const { parcelPerPage, totalParcels } = this.state
+
 
     return (
       <div className="main-container">
@@ -79,36 +155,36 @@ class Homepage extends Component {
         <div className="content-container">
           <div className="filter">
             <h3>Filter Parsel</h3>
-            <p>Berdasarkan Kategori</p>
+            <h5>Berdasarkan Isi :</h5>
             <div className="checkbox-container">
               <form action="kategori">
                 <div className="checkbox">
-                  <input type="checkbox" id="Makanan" />
+                  <input type="checkbox" id="Makanan" value={2} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(2) ? true : false} />
                   <label htmlFor="Makanan">Makanan</label>
                 </div>
                 <div className="checkbox">
-                  <input type="checkbox" id="Minuman" />
+                  <input type="checkbox" id="Minuman" value={3} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(3) ? true : false} />
                   <label htmlFor="Minuman">Minuman</label>
                 </div>
                 <div className="checkbox">
-                  <input type="checkbox" id="Pakaian" />
+                  <input type="checkbox" id="Pakaian" value={4} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(4) ? true : false} />
                   <label htmlFor="Pakaian">Pakaian</label>
                 </div>
                 <div className="checkbox">
-                  <input type="checkbox" id="Sembako" />
-                  <label htmlFor="Sembako">Kesehatan</label>
+                  <input type="checkbox" id="Kesehatan" value={5} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(5) ? true : false} />
+                  <label htmlFor="Kesehatan">Kesehatan</label>
                 </div>
                 <div className="checkbox">
-                  <input type="checkbox" id="Perabotan" />
-                  <label htmlFor="Perabotan">Sembako</label>
+                  <input type="checkbox" id="Sembako" value={6} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(6) ? true : false} />
+                  <label htmlFor="Sembako">Sembako</label>
                 </div>
                 <div className="checkbox">
-                  <input type="checkbox" id="Kesehatan" />
-                  <label htmlFor="Kesehatan">Perabotan</label>
+                  <input type="checkbox" id="Perabotan" value={7} onChange={(e) => this.onChangeFilter(+e.target.value)} checked={this.state.filterID.includes(7) ? true : false} />
+                  <label htmlFor="Perabotan">Perabotan</label>
                 </div>
               </form>
             </div>
-            <Button style={{ backgroundColor: "#7792A8", border: "none" }}>
+            <Button style={{ backgroundColor: "#7792A8", border: "none" }} onClick={this.onResetFilter}>
               Reset Filter
             </Button>
           </div>
@@ -182,7 +258,7 @@ class Homepage extends Component {
                 )
               })}
             </div>
-            <Pagination productPerPage={parcelPerPage} totalProduct={totalParcels} paginate={paginate} nextPage={nextPage} prevPage={prevPage} active={this.state.active} />
+            <Pagination productPerPage={parcelPerPage} totalProduct={totalParcels} paginate={this.paginate} nextPage={this.nextPage} prevPage={this.prevPage} active={this.state.active} />
           </div>
         </div>
         <div className="footer">
